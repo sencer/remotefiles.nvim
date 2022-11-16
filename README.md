cbf493685eb63a563474cf0629587cd3c4f1c004
Sencer Selcuk <sencerselcuk@gmail.com>
2022-11-16 14:22:29 -0500

    Refactor

--- README.md
+++ README.md
@@ -20,3 +20,5 @@ prints the file contents to stdout.
 `write_handler_fn` is optional. It accepts a single argument, filename; reads
 file contents from stdin and writes it to the file. If it is not provided, the
 file will be `readonly`.
+
+For an example, see `plugin/remotefiles.lua`.
--- plugin/remotefiles.lua
+++ plugin/remotefiles.lua
@@ -1,13 +1,20 @@
-require("remotefiles").register("scp://*", function(match)
-	local name = match:sub(7)
+if vim.g.loaded_remotefiles ~= nil then
+	return
+end
+
+local function parse_scp_filename(fname)
+	local name = fname:sub(7)
 	local psplit, _ = name:find("/")
 	local host = name:sub(1, psplit - 1)
 	local file = name:sub(psplit + 1)
+
+	return host, file
+end
+
+require("remotefiles").register("scp://*", function(filename)
+	local host, file = parse_scp_filename(filename)
 	return "ssh " .. host .. " cat " .. file
-end, function(match)
-	local name = match:sub(7)
-	local psplit, _ = name:find("/")
-	local host = name:sub(1, psplit - 1)
-	local file = name:sub(psplit + 1)
+end, function(filename)
+	local host, file = parse_scp_filename(filename)
 	return "ssh " .. host .. " tee " .. file .. ">/dev/null"
 end)
